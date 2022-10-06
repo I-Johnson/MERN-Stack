8c691b8152d149ca36a4a5d1422a243c2809476c
Johnson <subede.johnson@gmail.com>
2022-10-06 17:09:11 -0500

    res req & postman

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // Creating an express app. 
 const app = express()
 
-// routes request and response
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 
+
+// routes request and response
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to thee app'})
 })
 
 // listen for requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log('listening on port 4000')
 })
+
+process.env
