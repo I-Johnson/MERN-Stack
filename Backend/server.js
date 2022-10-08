2d8d11497148d9c39c72bc035dd696416dd097fe
Johnson <subede.johnson@gmail.com>
2022-10-08 09:41:08 -0400

    file arrangement

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,7 @@
-console.log('Hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = 3500
+
+const app = express()
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
